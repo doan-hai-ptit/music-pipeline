7cdbe43d86c6c4edebe1d668cc4a9213b269775e
Doãn Hải <doanhai200508@gmail.com>
2026-01-12 22:04:03 +0700

    Update top_countries.sql

--- dbt/models/top_countries.sql
+++ dbt/models/top_countries.sql
@@ -9,4 +9,3 @@ FROM {{ref('stg_music_events')}} m
 JOIN music_datalake.dim_countries d ON d.country_id = m.country_id
 GROUP BY d.country_id, d.country_name
 ORDER BY played_count DESC, d.country_name ASC
-LIMIT 10
